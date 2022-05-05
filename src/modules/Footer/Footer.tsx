8409dc689dd3a34c501c66d3bf20ea8d60f238c7
max7290599 <atroshschanka.maksim@gmail.com>
2022-05-05 13:34:07 +0300

    feat: #1_add footer

---
+++ src/modules/Footer/Footer.tsx
@@ -0,0 +1,39 @@
+import { Link } from 'react-router-dom';
+import style from './Footer.module.scss';
+
+const develops = [
+  {
+    id: 1,
+    name: 'Georgiy Beloklokov',
+    link: 'https://github.com/georgiybeloklokov',
+  },
+  {
+    id: 2,
+    name: 'Tatsiana Kastrubai',
+    link: 'https://github.com/kastrubait',
+  },
+  {
+    id: 3,
+    name: 'Maksim Atroschenkov',
+    link: 'https://github.com/max7290599',
+  },
+];
+
+const Footer = () => {
+  return (
+    <div className={style.footer}>
+      <div className={style.dev}>
+        {develops.map((develop) => (
+          <Link key={develop.id} className={style.github} to={develop.link}>
+            {develop.name}
+          </Link>
+        ))}
+      </div>
+      <Link className={style.rss} to="https://rs.school/react">
+        <span className={style.year}>22</span>
+      </Link>
+    </div>
+  );
+};
+
+export default Footer;
