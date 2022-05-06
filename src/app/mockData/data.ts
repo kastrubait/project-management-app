2dc6b55a97f5ed068d672a9546618906b035cf27
Tatsiana Kastrubai <tatsiana.kastrubai@gmail.com>
2022-05-06 23:21:37 +0300

    add: issue#17 mockData

---
+++ src/app/mockData/data.ts
@@ -0,0 +1,16 @@
+export const MOCK_DATA = {
+  boards: [
+    {
+      id: '1',
+      title: 'board #1',
+    },
+    {
+      id: '2',
+      title: 'board #2',
+    },
+    {
+      id: '3',
+      title: 'board #3',
+    },
+  ],
+};
