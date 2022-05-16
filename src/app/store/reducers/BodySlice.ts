9902f4e647cdb17ad778a1db80e7198b5fa1e1c6
Georgiy Beloklokov <georgiy.reus@gmail.com>
2022-05-16 22:23:24 +0300

    feat: init logic layer for Task

--- src/app/store/reducers/BodySlice.ts
+++ src/app/store/reducers/BodySlice.ts
@@ -1,6 +1,22 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { IBodyProps } from '../../Interfaces/Interfaces';
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { ApiService } from '../../Api/ApiService';
+import { IBodyProps, IUpdateProfile } from '../../Interfaces/Interfaces';
+import { RootState } from '../store';
 
+export const updateUserThunk = createAsyncThunk(
+  'header/updateUserThunk',
+  async ({ editProfileData }: IUpdateProfile, thunkAPI) => {
+    try {
+      const state = thunkAPI.getState() as RootState;
+      const response = await ApiService.createTasksById(state.header.userId, editProfileData);
+      return response;
+    } catch (err) {
+      if (err instanceof Error) {
+        return thunkAPI.rejectWithValue(err.message);
+      }
+    }
+  }
+);
 interface BodyState {
   bodyData: IBodyProps[];
 }
