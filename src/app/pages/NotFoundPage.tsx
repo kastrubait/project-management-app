5f7f2b671ae25cdcb801ba4b5befe94c8e0a8db0
max7290599 <atroshschanka.maksim@gmail.com>
2022-05-17 08:39:45 +0300

    fix: translate page

--- src/app/pages/NotFoundPage.tsx
+++ src/app/pages/NotFoundPage.tsx
@@ -1,16 +1,18 @@
+import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 import style from './NotFoundPage.module.scss';
 
 const NotFoundPage = () => {
+  const { t } = useTranslation();
   return (
     <div className={style.page}>
       <div className={style.notFound}>
         <div className={style.notFoundTitle}>
-          <h1>Oops!</h1>
-          <h2>404 - The Page can not be found</h2>
+          <h1>{t('Oops!')}</h1>
+          <h2>{t('404 - The Page can not be found')}</h2>
         </div>
         <Link className={style.link} to="/">
-          Go to homepage
+          {t('Go to homepage')}
         </Link>
       </div>
     </div>
