8364c09cef959be6867521e3bc95387e874f4f14
Maksim Atroschenko <atroshschanka.maksim@gmail.com>
2022-05-30 01:55:33 +0300

    fix: delete file

--- src/app/components/ScreenImage/ScreenImage.tsx
+++ src/app/components/ScreenImage/ScreenImage.tsx
@@ -1,11 +1,9 @@
-import style from './ScreenImage.module.scss';
-
 interface imageProps {
   src: string;
 }
 
 const ScreenImage = ({ src }: imageProps) => {
-  return <img className={style.img} src={src} />;
+  return <img src={src} />;
 };
 
 export default ScreenImage;
